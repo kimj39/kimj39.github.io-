dc5c81eb840184b53e67f87d41023076ff9b8865
kimj39 <ji.woo.kim@vanderbilt.edu>
2017-07-14 16:59:08 -0400

    asd

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -3,3 +3,23 @@
 $(document).ready(function(){
     $("#content").load("public/views/about.html #about");
 });
+
+$("miscNav").on('click', function() {
+  $("#content").load("public/views/misc.html #misc");
+});
+
+$("projectsNav").on('click', function() {
+  $("#content").load("public/views/projects.html #projects");
+});
+
+$("educationNav").on('click', function() {
+  $("#content").load("public/views/education.html #education");
+});
+
+$("aboutNav").on('click', function() {
+  $("#content").load("public/views/about.html #about");
+});
+
+$("resumeNav").on('click', function() {
+  $("#content").load("Resources/jiwookimCSresume.pdf");
+});
