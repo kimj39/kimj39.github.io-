4c7fcba91cdb2d023d9c59f384fec350fcf9f6c4
kimj39 <ji.woo.kim@vanderbilt.edu>
2017-07-14 14:43:09 -0400

    Asd

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,6 +1,6 @@
 
 /* Loads About Me when page is opened*/
 $(document).ready(function(){
-    $("#content").fadeIn("slow");
     $("#content").load("public/views/about.html #about");
+    $("#about").fadeIn("slow");
 });
