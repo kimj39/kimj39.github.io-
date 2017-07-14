623ee8bf9fa93cdffb437979f01713ecbbbb5ddc
kimj39 <ji.woo.kim@vanderbilt.edu>
2017-07-14 19:06:56 -0400

    asd

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,29 +1,29 @@
 
 /* Loads About Me when page is opened*/
 $(document).ready(function(){
-    $("#content").load("public/views/about.html #about");
+    $("#content").load("public/views/about.html #aboutPage");
+
+    $(".navOp").on('click', function(e){
+      console.log("lol");
+  });
 });
 
 // $("#miscNav").on('click', function() {
-//   $("#content").load("public/views/misc.html #misc");
+//   $("#content").load("public/views/misc.html #miscPage");
 // });
 
 // $("#projectsNav").on('click', function() {
-//   $("#content").load("public/views/projects.html #projects");
+//   $("#content").load("public/views/projects.html #projectsPage");
 // });
 
 // $("#educationNav").on('click', function() {
-//   $("#content").load("public/views/education.html #education");
+//   $("#content").load("public/views/education.html #educationPage");
 // });
 
 // $("#aboutNav").on('click', function() {
-//   $("#content").load("public/views/about.html #about");
+//   $("#content").load("public/views/about.html #aboutPage");
 // });
 
 // $("#resumeNav").on('click', function() {
 //   $("#content").load("Resources/jiwookimCSresume.pdf");
 // });
-
-$('.navOp').on('click', function(e){
-console.log('lol');
-});
