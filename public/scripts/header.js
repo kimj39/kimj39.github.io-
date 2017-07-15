1ee965ba21b1bf5beb13191118971cb9d31846d0
kimj39 <ji.woo.kim@vanderbilt.edu>
2017-07-14 21:29:37 -0400

    asd

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 /* Loads corresponding page when selected in Nav Bar */
 $(".navbar").on('click', function(e){
   var myClass = e.target.className;
-  if (myClass === "navOp" || myClass === "navText" || myClass === "icon" ) {
+  if (myClass === "navOp" || myClass === "navText") {
     var myId = e.target.id;
     if (myId !== "resume"){
       $("#content").load("public/views/" + myId + ".html #" + myId + "Page");
