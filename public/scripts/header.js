4dfaefb95ddf3b0b0065d2cdda72587c5675a9ee
kimj39 <ji.woo.kim@vanderbilt.edu>
2017-07-14 20:15:28 -0400

    asd

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -4,30 +4,16 @@ $(document).ready(function(){
     $("#content").load("public/views/about.html #aboutPage");
 });
 
-// $("#miscNav").on('click', function() {
-//   $("#content").load("public/views/misc.html #miscPage");
-// });
-
-// $("#projectsNav").on('click', function() {
-//   $("#content").load("public/views/projects.html #projectsPage");
-// });
-
-// $("#educationNav").on('click', function() {
-//   $("#content").load("public/views/education.html #educationPage");
-// });
-
-// $("#aboutNav").on('click', function() {
-//   $("#content").load("public/views/about.html #aboutPage");
-// });
-
-// $("#resumeNav").on('click', function() {
-//   $("#content").load("Resources/jiwookimCSresume.pdf");
-// });
+$("#resume").on('click', function() {
+  $("#content").load("Resources/jiwookimCSresume.pdf");
+});
 
 $(".navbar").on('click', function(e){
   var myClass = e.target.className;
   if (myClass === "navOp") {
     var myId = e.target.id;
-    $("#content").load("public/views/" + myId + ".html #" + myId + "Page");
+    if (myId !== "resume"){
+      $("#content").load("public/views/" + myId + ".html #" + myId + "Page");
+    }
   }
 });
